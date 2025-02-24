c477fa6380d081da8c042f4e1c6637e5fc845355
yrrel <fulejohnlerry1112@gmail.com>
2025-02-24 14:41:56 +0800

    rework, added api method and js

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,url_for,redirect, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -12,19 +12,46 @@ db=SQLAlchemy(app)
 
 
 #TO IMPORT DB
-class staff(db.Model):
+class Staff(db.Model):
     __tablename__ = 'staff'
     uid = db.Column(db.Integer, primary_key=True)
     fname = db.Column(db.String(255))
     lname = db.Column(db.String(255))
     age = db.Column(db.Integer)
 
+#get all staff
+@app.route('/api/staff',methods=['GET'])
+def get_all_staff():
+    staffs = Staff.query.all()
+    return jsonify([{"uid":s.uid,"fname":s.fname,"lname":s.lname,"age":s.age}for s in staffs])
 
+#view all information
 @app.route('/')
 def index():
-    staffs = staff.query.all()
+    staffs = Staff.query.all()
     return render_template('index.html', staffs = staffs)
 
+#insert a new staff
+@app.route('/api/staff', methods=['POST'])
+def create_staff():
+    data = request.json  # Ensure JSON data is received
+    if not data:
+        return jsonify({"error": "Invalid input"}), 400
+    
+    new_staff = Staff(fname=data['fname'], lname=data['lname'], age=data['age'])
+    db.session.add(new_staff)
+    db.session.commit()
+    
+    return jsonify({"message": "Staff added successfully"}), 201
+
+
+
+
+@app.route('/delete/<int:uid>', methods =['DELETE'])
+def delete(uid):
+    Staff_delete = Staff.query.get(uid)
+
+    return {"message": "delete successfully"}
 
 
 if __name__ == '__main__':
